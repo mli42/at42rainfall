11422879aae0930c59abd9b9e2b00e092892cc81
Baptiste Fraikin <fraikin.baptiste@gmail.com>
2022-05-16 15:46:10 +0200

    whe

---
+++ level6/main.c
@@ -0,0 +1,22 @@
+
+
+void n()
+{
+  system("/bin/cat /home/user/level7/.pass");
+
+}
+
+void m()
+{
+  puts("Nope");
+}
+
+int main() {
+
+  str =malloc(64);
+    str1 = malloc(4);
+    coucou = m
+    strcpy();
+
+
+}
