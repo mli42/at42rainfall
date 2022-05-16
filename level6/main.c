7d7f1b7df5820e8cca7ac2d88206d37ab71d6d6c
Baptiste Fraikin <fraikin.baptiste@gmail.com>
2022-05-16 18:41:22 +0200

    whe

--- level6/main.c
+++ level6/main.c
@@ -11,12 +11,12 @@ void m()
   puts("Nope");
 }
 
-int main() {
+int main(int argc, char **argv) {
 
+  char *str;
+  char *str1;
   str =malloc(64);
-    str1 = malloc(4);
-    coucou = m
-    strcpy();
-
-
+  str1 = malloc(4);
+  coucou = m
+    strcpy(str, argv[1]);
 }
