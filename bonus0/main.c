edb78691938ec2dd3c5090e4844ab23dce61e924
Baptiste Fraikin <fraikin.baptiste@gmail.com>
2022-05-19 11:25:25 +0200

    whe

--- bonus0/main.c
+++ bonus0/main.c
@@ -1,12 +1,35 @@
 
-int p() {
+int p(char *str, char *sepa) {
 
+  char *to_read = "";
+  puts(sepa);
+  read(stdin, to_read, 0x1000);
+  index = strchr('\n', to_read);
+  to_read[index] = '\0';
+  strncpy(str, to_read, 20);
 }
-int pp() {
+int pp(char *str) {
 
+  char *sepa = " - \n";
+  char *new= "";
+  char *blank= " ";
+  char *first;
+  char *second;
+
+  p(first, sepa);
+  p(second, sepa);
+
+  strcpy(first, second);
+  strlen(first);
+
+  str = strcat(first, second)
 }
 
-int main() {
+int main(int argc, char argv**) {
 
+  char *str= "";
+  pp(str);
 
+  puts(str);
+  return 0;
 }
