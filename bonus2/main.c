67ea502df176e20eb1703e0685d0893d9a5375a0
Baptiste Fraikin <fraikin.baptiste@gmail.com>
2022-05-19 17:58:34 +0200

    whe

--- bonus2/main.c
+++ bonus2/main.c
@@ -12,17 +12,14 @@ int main(int argc, char **argv) {
   strncpy("", argv[1], 0x28);
   strncpy("", argv[2], 0x20);
 
-  if  (getenv("LANG") == 0)
-    return ;
-  if (memcpm({LANG}, "fi",2) == 1)
+  if  (getenv("LANG") != 0)
   {
-    language = 1;
-    return;
+    if (memcpm({LANG}, "fi",2) == 1)
+      language = 1;
+    if (memcpm({LANG}, "nl",2) == 0)
+      language = 2;
   }
-  if (memcpm({LANG}, "nl",2) == 0)
-    language = 2;
-  else
-    return;
   greetuser();
 
+  return;
 }
