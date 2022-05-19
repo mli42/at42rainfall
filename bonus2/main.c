2688ef075560768d3d1d874d05ba926fcb1567ce
mli42 <mli@student.42.fr>
2022-05-19 18:39:22 +0200

    b2: greet user begin

--- bonus2/main.c
+++ bonus2/main.c
@@ -1,25 +1,48 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
-void greetuser() {
+void greetuser(char *str) {
+  int lang = 0; /* {language} */
+  char *hello = NULL;
+  char *str1 = NULL;
+
+  if (lang == 1) {
+    hello = "Hyvää päivää ";
+
+  }
+  else if (lang == 2) {
+    hello = "Goedemiddag! ";
+
+  }
+  else if (lang != 0) {
+    hello = "Hello ";
+
+  }
+
+  strcat(str1, hello);
+  puts(str1);
 
 }
 
 int main(int argc, char **argv) {
   char language = 0;
 
-  if (argc == 3)
-    return; 
+  if (argc != 3)
+    return 1;
   strncpy("", argv[1], 0x28);
   strncpy("", argv[2], 0x20);
 
-  if  (getenv("LANG") != 0)
+  char *lang = getenv("LANG");
+  if (lang != NULL)
   {
-    if (memcpm({LANG}, "fi",2) == 1)
+    if (memcmp(lang, "fi", 2) == 1)
       language = 1;
-    if (memcpm({LANG}, "nl",2) == 0)
+    if (memcmp(lang, "nl", 2) == 0)
       language = 2;
   }
   greetuser();
 
-  return;
+  return 0;
 }
